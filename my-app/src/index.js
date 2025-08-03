bf9f354cf5502f6279d48d520e3bc68d1928652b
IRFAN ANSARI <148984183+Irfansarii@users.noreply.github.com>
2025-08-03 20:43:53 +0530

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,12 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { BrowserRouter } from 'react-router-dom';
 
 AOS.init();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <BrowserRouter basename="/Nirogyan-Assessment">
     <App />
+   </BrowserRouter>
   </React.StrictMode>
 );
 
